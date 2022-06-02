259ce7cda5cf288bf1a27e43a23f37993bca304d
fabianafreitasmendes <fabianafreitasmendes@192.168.0.103>
2022-06-02 16:07:54 -0300

    Modificações na estrutura do repositório

--- ExemplosCodigoAula/src/aula02/EstruturaDecisao.java
+++ ExemplosCodigoAula/src/aula02/EstruturaDecisao.java
@@ -1,4 +1,4 @@
-package aula01;
+package aula02;
 
 import java.util.Scanner;
 
---
+++ ExemplosCodigoAula/src/aula02/HelloWorldComentado.java
@@ -0,0 +1,21 @@
+package aula02;
+
+/* Comentarios com mais de uma linha sao feitos assim
+ * Evite usar acentos no seu programa
+ * Blocos de instrucoes sao iniciados com { e finalizados com }
+ * Nomes de classes iniciam com letra maiuscula
+ */
+
+public class HelloWorldComentado {
+	//O metodo main e responsavel pela execucao
+	public static void main(String[] args) {
+		/* Strings precisam vir entre aspas
+		 * Nomes de variaveis iniciam com letra minuscula
+		 * Java e case sensitive, ou seja, str e diferente de sTR
+		 * O operador de atribuicao e =, igual em C
+		 */
+		String str = ("Hello World");
+		System.out.println(str);
+		// Uma instrucao deve ser encerrada com ;
+	}
+}
