56fef22394ca007b2ab8e321a6d0c76c2772a94a
fabianafreitasmendes <fabianafreitasmendes@192.168.0.103>
2022-06-02 12:34:57 -0300

    Inclusão dos códigos referentes a estrutura de repetição e estrutura sequencial

---
+++ ExemplosCodigoAula/src/aula01/EstruturaRepeticao.java
@@ -0,0 +1,20 @@
+package aula01;
+
+import java.util.Scanner;
+
+public class EstruturaRepeticao {
+
+	public static void main(String[] args) {
+		int vetInt[] = new int[10];
+		Scanner entrada = new Scanner(System.in);
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Digite um numero inteiro:");
+			vetInt[i] = entrada.nextInt();
+		}
+		entrada.close();
+		for(int i = 0; i < 10; i++) {
+			System.out.print(vetInt[i] + " ");
+		}
+	}
+}
+
