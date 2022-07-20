635bf141eacb39e0412ee73ae09bebb7465f946b
fabianafreitasmendes <fabianafreitasmendes@192.168.0.102>
2022-07-19 21:03:16 -0300

    Inclusão dos exemplos de codigo da Aula 20

---
+++ ExemplosCodigoAula/src/aula20/HelloWorld.java
@@ -0,0 +1,11 @@
+package aula20;
+
+import javax.swing.JFrame;
+
+public class HelloWorld {
+	public static void main(String[] a) { 
+		JFrame f = new JFrame("Hello world!"); 
+		f.setVisible(true); 
+	}
+}
+
