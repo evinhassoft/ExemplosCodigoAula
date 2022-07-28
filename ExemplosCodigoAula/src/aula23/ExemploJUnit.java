ae8bb58111e0fbb8302a910e75b912ba084452ff
fabianafreitasmendes <fabianafreitasmendes@192.168.0.102>
2022-07-28 18:20:27 -0300

    Códigos presentes nos slides da aula 23 - Testes de Software

---
+++ ExemplosCodigoAula/src/aula23/ExemploJUnit.java
@@ -0,0 +1,34 @@
+/*
+ * As classes de teste devem fica em uma pasta que contem
+ * apenas apenas esse tipo de classe. Nao as coloque
+ * dentro do pacote controle
+ */
+package aula23;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ExemploJUnit {
+
+	@Test
+	void testCheckCPF() {
+		String valorCPFErrado = "123.456.789-00";
+		String valorCPFCorreto = "12345678900";
+		assertTrue(ControleInformacoesPessoais.checkCPF(
+				valorCPFCorreto));
+		assertFalse(ControleInformacoesPessoais.checkCPF(
+				valorCPFErrado));
+	}
+
+	@Test
+	void testCheckTel() {
+		String valorTelErrado = "(61) 9 9999-9999";
+		String valorTelCorreto = "61999999999";
+		assertTrue(ControleInformacoesPessoais.checkTel(
+				valorTelCorreto));
+		assertFalse(ControleInformacoesPessoais.checkTel(
+				valorTelErrado));
+	}
+
+}
